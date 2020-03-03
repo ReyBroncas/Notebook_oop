d49e23bc1d7b580d85a657f5beb0964e5c65a67f
ReyBroncas <ReyBroncas@github.com>
2020-03-03 13:10:51 +0200

    added a test

--- notebook_test.py
+++ notebook_test.py
@@ -1,5 +1,6 @@
 from notebook import Notebook, Note
 from menu import Menu
+import pprint
 
 
 def display():
@@ -9,6 +10,10 @@ def display():
         for k, v in vars(obj).items():
             if callable(v):
                 print(f'──> a method "{k}", which: {v.__doc__}')
+    for obj in obj_list:
+        print('─' * 40 + f'\n\nPlain list of methods an attributes of\
+ an object {obj} :')
+        pprint.pprint(sorted(dir(obj)))
 
 
 if __name__ == '__main__':
